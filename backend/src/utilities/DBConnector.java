613fd6355e44a867d7402905b02df105f77a5e60
MiloOyaguezKarlsson <mioy97001@utb.vaxjo.se>
2018-06-05 12:34:39 +0200

    config file with static strings for tests and db

--- backend/src/utilities/DBConnector.java
+++ backend/src/utilities/DBConnector.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBConnector {
     public static Connection connect(String database) throws SQLException, ClassNotFoundException {
         String url = "jdbc:mysql://localhost/" + database;
-        String user = "root";
-        String pw = "";
+        String user = config.Config.DB_USERNAME;
+        String pw = config.Config.DB_PASSWORD;
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(url, user, pw);
     }
